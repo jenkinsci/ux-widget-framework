0688590aa9ddd414941d67432dfcebeff16feb4b
Josh McDonald <josh@joshmcdonald.info>
2018-08-22 18:41:31 +1000

    reflector-tests * Add second set of json to inspection util

--- debug-types.js
+++ debug-types.js
@@ -1,97 +1,101 @@
-const src = require('./test/types-sample.json');
+const src1 = require('./test/types-sample.json');
+const src2 = require('./test/self-types.json');
 
 const KindString = {
-    Class              : 'Class',
-    Constructor        : 'Constructor',
-    Enumeration        : 'Enumeration',
-    EnumerationMember  : 'Enumeration member',
-    ExternalModule     : 'External module',
-    Function           : 'Function',
-    Interface          : 'Interface',
-    Method             : 'Method',
-    Module             : 'Module',
-    ObjectLiteral      : 'Object literal',
-    Property           : 'Property',
-    TypeAlias          : 'Type alias',
-    Variable           : 'Variable',
+    Class: 'Class',
+    Constructor: 'Constructor',
+    Enumeration: 'Enumeration',
+    EnumerationMember: 'Enumeration member',
+    ExternalModule: 'External module',
+    Function: 'Function',
+    Interface: 'Interface',
+    Method: 'Method',
+    Module: 'Module',
+    ObjectLiteral: 'Object literal',
+    Property: 'Property',
+    TypeAlias: 'Type alias',
+    Variable: 'Variable',
 };
 
-const kindCount = new Map(); // kindString -> count
-const propKindCounts = new Map(); // propName -> (kindString -> count)
+function dump(src) {
+    const kindCount = new Map(); // kindString -> count
+    const propKindCounts = new Map(); // propName -> (kindString -> count)
 
-function simplify(node) {
+    function simplify(node) {
 
+        const r = {};
+        r.id = node.id;
+        r.name = node.name;
+        const kindString = node.kindString;
+        // r.kind = node.kind;
+        r.kindString = kindString;
 
+        if (kindString) {
 
-    const r = {};
-    r.id = node.id;
-    r.name = node.name;
-    const kindString = node.kindString;
-    // r.kind = node.kind;
-    r.kindString = kindString;
+            kindCount.set(kindString, (kindCount.get(kindString) || 0) + 1);
 
-    if (kindString) {
-     
-        kindCount.set(kindString, (kindCount.get(kindString)||0) + 1);
-       
-        for (const propName in node) {
-            if (!propKindCounts.has(propName)) {
-                propKindCounts.set(propName, new Map());
+            for (const propName in node) {
+                if (!propKindCounts.has(propName)) {
+                    propKindCounts.set(propName, new Map());
+                }
+                const kindCountsForThisProp = propKindCounts.get(propName);
+                kindCountsForThisProp.set(kindString, (kindCountsForThisProp.get(kindString) || 0) + 1);
             }
-            const kindCountsForThisProp = propKindCounts.get(propName);
-            kindCountsForThisProp.set(kindString, (kindCountsForThisProp.get(kindString) || 0)+1);
         }
+
+        let c = [];
+
+        for (const child of node.children || []) {
+            c.push(simplify(child));
+        }
+
+        if (kindString === KindString.Class ||
+            kindString === KindString.Interface ||
+            kindString === KindString.ObjectLiteral ||
+            kindString === KindString.Enumeration) {
+            c = [];
+        }
+
+        if (c.length) {
+            r.children = c;
+            return r;
+        }
+
+        return `${r.kindString} ${r.name}`;
     }
 
-    let c = [];
+    console.log();
+    console.log(`Dumping type info for ${src.name}`);
+    console.log('=============================================================');
 
-    for (const child of node.children || []) {
-        c.push(simplify(child));
-    }
+    const cleaned = simplify(src);
 
-    if (kindString === KindString.Class ||
-        kindString === KindString.Interface ||
-        kindString === KindString.ObjectLiteral ||
-        kindString === KindString.Enumeration) {
-        c = [];
-    }
+    // console.log(JSON.stringify(cleaned, null, 4));
 
-    if (c.length) {
-        r.children = c;
-        return r;
-    }
-
-    return `${r.kindString} ${r.name}`;
-}
-
-console.log();
-console.log(`Dumping type info for ${src.name}`);
-console.log('=============================================================');
-
-const cleaned = simplify(src);
-
-// console.log(JSON.stringify(cleaned, null, 4));
-
-console.log();
-console.log('All Kinds:');
-for (const kind of kindCount.keys()) {
-    console.log(` * ${kind} - ${kindCount.get(kind)}`);
-}
-
-console.log();
-console.log('All Props:');
-const propGrid = [];
-for (const propName of propKindCounts.keys()) {
-
-    const propRow = { propName };
-    const matchingKindCounts = propKindCounts.get(propName);
-
+    console.log();
+    console.log('All Kinds:');
     for (const kind of kindCount.keys()) {
-        if (matchingKindCounts.has(kind)) {
-            propRow[kind] = matchingKindCounts.get(kind) === kindCount.get(kind) ? 'ALL' : 'some';
-        }
+        console.log(` * ${kind} - ${kindCount.get(kind)}`);
     }
-    
-    propGrid.push(propRow);
+
+    console.log();
+    console.log('All Props:');
+    const propGrid = [];
+    for (const propName of propKindCounts.keys()) {
+
+        const propRow = { propName };
+        const matchingKindCounts = propKindCounts.get(propName);
+
+        for (const kind of kindCount.keys()) {
+            if (matchingKindCounts.has(kind)) {
+                propRow[kind] = matchingKindCounts.get(kind) === kindCount.get(kind) ? 'ALL' : 'some';
+            }
+        }
+
+        propGrid.push(propRow);
+    }
+    console.table(propGrid);
 }
-console.table(propGrid);
+
+dump(src1);
+dump(src2);
