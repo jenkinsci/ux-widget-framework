5606fa63bbf2184151a58b7503c52813c187d610
Josh McDonald <josh@joshmcdonald.info>
2018-10-05 10:43:15 +1000

    task/cleanup-docgen-errs * Make sure we have a working index and clean up the dist a little

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export {
+    UXWidget,
+    WidgetDescription,
+} from './WidgetComponent';
+
+export {
+    Emitter,
+    EventSource,
+    Handler,
+    HostEvent,
+    Signal,
+    Subscription
+} from './HostEvents';
+
