7a0b408e121364230deb98fb4ef71d73fe5b64a7
Josh McDonald <josh@joshmcdonald.info>
2018-11-23 16:51:47 +1000

    port-JENKINS-53127 * Make story snapshots jest test automagic so it doesn't need to be maintained

--- packages/pipeline-graph/src/test/PipelineGraphSnapshots.spec.ts
+++ packages/pipeline-graph/src/test/PipelineGraphSnapshots.spec.ts
@@ -1,64 +1,17 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 
-import {
-    renderFlatPipeline,
-    renderMultiParallelPipeline,
-    renderMultiStageParallel,
-    renderMultiStageSpacing,
-    renderEdgeCases1,
-    renderLongNames,
-    renderWithDuplicateNames,
-    renderFlatPipelineFat,
-    renderListenersPipeline,
-    renderParallelPipeline,
-    renderParallelPipelineDeep,
-} from '../stories/PipelineGraphStoriesImpl';
+import * as stories from '../stories/PipelineGraphStoriesImpl';
 
 describe('PipelineGraph Renderer Storybook snapshots', () => {
-    test('renderFlatPipeline', () => {
-        expect(treeForStory(renderFlatPipeline)).toMatchSnapshot();
-    });
-
-    test('renderMultiParallelPipeline', () => {
-        expect(treeForStory(renderMultiParallelPipeline)).toMatchSnapshot();
-    });
-
-    test('renderMultiStageParallel', () => {
-        expect(treeForStory(renderMultiStageParallel)).toMatchSnapshot();
-    });
-
-    test('renderMultiStageSpacing', () => {
-        expect(treeForStory(renderMultiStageSpacing)).toMatchSnapshot();
-    });
-
-    test('renderEdgeCases1', () => {
-        expect(treeForStory(renderEdgeCases1)).toMatchSnapshot();
-    });
-
-    test('renderLongNames', () => {
-        expect(treeForStory(renderLongNames)).toMatchSnapshot();
-    });
-
-    test('renderWithDuplicateNames', () => {
-        expect(treeForStory(renderWithDuplicateNames)).toMatchSnapshot();
-    });
-
-    test('renderFlatPipelineFat', () => {
-        expect(treeForStory(renderFlatPipelineFat)).toMatchSnapshot();
-    });
-
-    test('renderListenersPipeline', () => {
-        expect(treeForStory(renderListenersPipeline)).toMatchSnapshot();
-    });
-
-    test('renderParallelPipeline', () => {
-        expect(treeForStory(renderParallelPipeline)).toMatchSnapshot();
-    });
-
-    test('renderParallelPipelineDeep', () => {
-        expect(treeForStory(renderParallelPipelineDeep)).toMatchSnapshot();
-    });
+    for (const name in stories) {
+        if (name.indexOf('render') === 0) {
+            const f = (stories as any)[name];
+            test(name, () => {
+                expect(treeForStory(f)).toMatchSnapshot();
+            });
+        }
+    }
 });
 
 function treeForStory(f: () => React.ReactElement<any>): renderer.ReactTestRendererJSON {
